6db88c6fe0dcf78d62d9c3e029adf4738321b096
oribanda <oribanda2@gmail.com>
2020-05-28 08:37:57 +0900

    自動更新機能

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,28 @@
+class Api::MessagesController < ApplicationController
+  def index
+    # ルーティングでの設定によりparamsの中にgroup_idというキーでグループのidが入るので、これを元にDBからグループを取得する
+    group = Group.find(params[:group_id])
+    # ajaxで送られてくる最後のメッセージのid番号を変数に代入
+    last_message_id = params[:id].to_i
+    # 取得したグループでのメッセージ達から、idがlast_message_idよりも新しい(大きい)メッセージ達のみを取得
+    @messages = group.messages.includes(:user).where("id > ?", last_message_id)
+  end
+  
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
     #キーワードがなければnilを返す
-    return nili if params[:keyword] == ""
+    return nil if params[:keyword] == ""
     #その文字を含むユーザーを探してきて、@usersに代入。ただしログインしている自分は除く。10人まで。
     @users = User.where( ['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
     respond_to do |format|
